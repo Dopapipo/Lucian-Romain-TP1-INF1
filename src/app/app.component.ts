82aa8b83e700a5449477544fe7c904578ba826d3
Romain <romainjacob012@gmail.com>
2024-09-25 12:22:16 +0200

    feature:square

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./header/header.component";
 import {FooterComponent} from "./footer/footer.component";
+import {SquareComponent} from "./square/square.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, SquareComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
